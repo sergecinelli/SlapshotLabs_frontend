26ac7c9bb9a6b68bc6cb0f47c16bc83f38b6ce4c
Dmytro Fedchenko <dmytro_fedchenko@outlook.com>
2025-09-24 15:55:17 +0300

    feat: Add custom color palettes and utility classes for theming
    - Defined custom primary and secondary color palettes in styles.scss.
    - Updated the HTML theme configuration to use custom palettes.
    - Set custom background and text colors for the body.
    - Added CSS variables for easy access to custom colors.
    - Created utility classes for custom colors in Tailwind CSS.
    - Extended Tailwind configuration to include custom color options.

--- hockey-baseball-app/tailwind.config.js
+++ hockey-baseball-app/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom': {
+          'primary': '#cf142b',     // Primary red
+          'dark': '#090b0c',        // Dark text
+          'secondary': '#778f9c',   // Secondary background
+          'white': '#ffffff',       // White background
+        }
+      }
+    },
   },
   plugins: [],
   corePlugins: {
