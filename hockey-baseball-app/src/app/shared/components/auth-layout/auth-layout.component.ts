af51b5f4e7f623dff370b428cf9edce71c306688
bohdanovdmytro <dmytro.bohdanov@gentlecode.com>
2026-03-06 03:13:25 +0200

    feat: enhance authentication components with icons and layout improvements
    - Added authentication icons to Forgot Password, Reset Password, Sign In, Sign Up, and Invitation components for better visual cues.
    - Updated layout styles in auth-related components to improve user experience and consistency.
    - Adjusted card padding and column gaps in SCSS for a more polished appearance across the application.
    - Refactored HTML structure in the Auth Layout component for improved readability and maintainability.

--- hockey-baseball-app/src/app/shared/components/auth-layout/auth-layout.component.ts
+++ hockey-baseball-app/src/app/shared/components/auth-layout/auth-layout.component.ts
@@ -1,10 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-layout',
   imports: [],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'auth-layout' },
 })
 export class AuthLayoutComponent {
   titleText = input('');
